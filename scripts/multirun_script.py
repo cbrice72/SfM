49aa1c0f9509bfb98e67cbad13c1b99bc4083d10
Christian Brice <brice.c.aa@m.titech.ac.jp>
2024-10-02 15:03:20 +0900

    folder renaming

--- scripts/multirun_script.py
+++ scripts/multirun_script.py
@@ -37,11 +37,11 @@ shortcuts = {
         {"-n": "10"},
         ["-i"],
         [
-            ["/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/baseline/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/darkness/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-90/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-45/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-0/color/extracted"
+            ["/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/baseline/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/darkness/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-90/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-45/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-0/color/extracted"
             ]
         ]),
     "2024-10-1 Videos": Shortcut(
@@ -49,16 +49,16 @@ shortcuts = {
         {},
         ["-i"],
         [
-            ["/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/baseline/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/baseline/depth/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/darkness/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/darkness/depth/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-90/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-90/depth/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-45/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-45/depth/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-0/color/extracted",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-0/depth/extracted"
+            ["/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/baseline/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/baseline/depth/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/darkness/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/darkness/depth/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-90/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-90/depth/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-45/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-45/depth/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-0/color/extracted",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-0/depth/extracted"
             ]
         ]),
     "2024-10-1 SfM": Shortcut(
@@ -66,11 +66,11 @@ shortcuts = {
         {"--use-defaults": ""},
         ["-i"],
         [
-            ["/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/baseline/color/images",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/darkness/color/images",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-90/color/images",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-45/color/images",
-            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense_Indirect_Lighting_Experiment/light-0/color/images"
+            ["/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/baseline/color/images",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/darkness/color/images",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-90/color/images",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-45/color/images",
+            "/mnt/c/Users/brice/Desktop/Projects/LIBRA/Archive/2024-10-01_RealSense-Indirect-Lighting-Experiment/light-0/color/images"
             ]
         ])
 }
